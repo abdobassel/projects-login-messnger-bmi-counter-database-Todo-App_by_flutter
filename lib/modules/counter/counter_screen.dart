a899f285c743e47e24fc8024527294d606c1077a
abdobassel <abdobassel@gmail.com>
2023-11-16 23:50:40 +0200

    in counter screen =>bloc provider and consumer added and object from cubit class

--- lib/modules/counter/counter_screen.dart
+++ lib/modules/counter/counter_screen.dart
@@ -1,42 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:udemy_mansour/modules/counter/bloc_counter/cubit.dart';
+import 'package:udemy_mansour/modules/counter/bloc_counter/states.dart';
 
 class MyCounter extends StatelessWidget {
   const MyCounter({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Counter'),
+    CounterCubit counterCubit = BlocProvider.of(context);
+    return BlocProvider(
+      create: (context) => CounterCubit(InitCounterState()),
+      child: BlocConsumer<CounterCubit, CounterStates>(
+        listener: (BuildContext context, CounterStates state) {},
+        builder: (context, state) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Counter'),
+            ),
+            body: Center(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    counterCubit.plus_counter();
+                  },
+                  icon: Icon(
+                    Icons.add,
+                    color: Colors.grey,
+                  ),
+                  iconSize: 30,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Text(
+                    '',
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    counterCubit.minuscounter();
+                  },
+                  icon: Icon(
+                    Icons.minimize,
+                  ),
+                  iconSize: 30,
+                )
+              ],
+            )),
+          );
+        },
       ),
-      body: Center(
-          child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.add,
-              color: Colors.grey,
-            ),
-            iconSize: 30,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Text(
-              '',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.minimize,
-            ),
-            iconSize: 30,
-          )
-        ],
-      )),
     );
   }
 }
